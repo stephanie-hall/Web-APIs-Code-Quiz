3a3adc66f32f23de470093beb6898c19e1cbb3f3
Stephanie Hall <Stephaniehall41@gmail.com>
2020-08-10 21:51:19 -0400

    update JS

---
+++ Assets/questions.js
@@ -0,0 +1,28 @@
+// list of all questions, choices, and answers
+var questions = [
+    {
+      title: "Which character is used to indicate an end tag?",
+      choices: ["^", "/", "*", "<"],
+      answer: "/"
+    },
+    {
+      title: "What does CSS stand for?.",
+      choices: ["Computer Style Sheets", "Colorful Style Sheets", "Creative Style Sheets", "Cascading Style Sheets"],
+      answer: "Cascading Style Sheets"
+    },
+    {
+      title: "How do you create a function in JavaScript?",
+      choices: ["function = myFunction()", "function myFunction()", "function:myFunction()", "all of the above"],
+      answer: "function myFunction()"
+    },
+    {
+      title:"The Bootstrap grid system is based on how many columns?",
+      choices: ["12", "6", "9", "3"],
+      answer: "12"
+    },
+    {
+      title: "Which sign does jQuery use as a shortcut for jQuery?",
+      choices: ["The $ sign", "The ? sign", "The % sign", "The # sign"],
+      answer: "The $ sign"
+    }
+];
